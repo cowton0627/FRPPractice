8a5f99f70f602e080a9a3752a388e8158c977783
cowton0627 <83654992+cowton0627@users.noreply.github.com>
2021-12-17 22:06:28 +0800

    Initial Commit

---
+++ FRPPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FRPPractice
+//
+//  Created by Chun-Li Cheng on 2021/12/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
